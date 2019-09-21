944febf53b005143a147dc4ab6d02b34f8636db8
Zalasento <wzs112233@gmail.com>
2019-09-21 16:35:40 +0800

    id 9,a faster solution

--- 9.palindrome-number.java
+++ 9.palindrome-number.java
@@ -4,11 +4,20 @@
  * [9] Palindrome Number
  */
 class Solution {
+    /*
+     * study the solution
+     * https://leetcode.com/problems/palindrome-number/discuss/319913/Java-runtime-
+     * 6ms-faster-than-100
+     */
     public boolean isPalindrome(int x) {
         if (x < 0)
             return false;
-        String s = Integer.toString(x);
-        String reverse = new StringBuffer(s).reverse().toString();
-        return s.equals(reverse);
+        int reverse = 0, tmp = x;
+        while (tmp > 0) {
+            reverse = reverse * 10;
+            reverse += tmp % 10;
+            tmp /= 10;
+        }
+        return reverse == x;
     }
 }
