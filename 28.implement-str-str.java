d728f97489ee7ad324e1142db4ea02deb9b68c37
Zalasento <wzs317@hotmail.com>
2019-09-25 16:43:44 +0800

    (id 28)pass the test case, but I forget verity the needle's length in th program, so the case "aaa" "aaaa" don't pass

---
+++ 28.implement-str-str.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=28 lang=java
+ *
+ * [28] Implement strStr()
+ */
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.equals(""))
+            return 0;
+        int indexNeedle = 0;
+        int index = -1;
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(indexNeedle)) {
+                if (index == -1)
+                    index = i;
+                indexNeedle++;
+                if (indexNeedle == needle.length())
+                    return index;
+            } else {
+                index = -1;
+                indexNeedle = 0;
+            }
+        }
+        return index;
+    }
+}
