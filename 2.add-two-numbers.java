9032ded1a5465b2b363007c2547221435cd807c8
Zalasento <wzs317@hotmail.com>
2019-09-26 21:36:33 +0800

    (id 2) do not solve
    can't pass case [0] [7,3]

---
+++ 2.add-two-numbers.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=2 lang=java
+ *
+ * [2] Add Two Numbers
+ */
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode p = l1, q = l2;
+        boolean carry = false;
+        while (p.next != null && q.next != null) {
+            p.val += q.val;
+            if (carry) {
+                p.val++;
+                carry = false;
+            }
+            if (p.val > 9) {
+                carry = true;
+                p.val -= 10;
+            }
+            p = p.next;
+            q = q.next;
+        }
+        if (q.next != null && p.next == null) {
+            p.next = q.next;
+        } else if (q.next == null && p.next == null) {
+            p.val += q.val;
+            if (carry) {
+                p.val++;
+            }
+            if (p.val > 9) {
+                p.val -= 10;
+                p.next = new ListNode(1);
+            }
+            return l1;
+        } else
+            p.next = p.next;
+        p = p.next;
+        while (p.next != null) {
+            if (carry) {
+                p.val++;
+                carry = false;
+            } else
+                return l1;
+            if (p.val > 9) {
+                carry = true;
+                p.val -= 10;
+            }
+            p = p.next;
+        }
+        if (carry) {
+            p.next = new ListNode(1);
+
+        }
+        return l1;
+    }
+}
