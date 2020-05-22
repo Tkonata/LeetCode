014ba525fbee7d4087cb51b5cd18928b186ce396
wakou <wzs317@hotmail.com>
2020-05-22 21:48:49 +0800

    (id 1) 排序+双指针

--- 1.two-sum.cs
+++ 1.two-sum.cs
@@ -20,6 +20,43 @@ public class Solution
         }
         return new int[0];
     }
+
+    public int[] TwoPointer(int[] nums, int target) {
+        int[] numsCopy = new int[nums.Length];
+        Array.Copy(nums, numsCopy, nums.Length);
+        Array.Sort(numsCopy);
+        var left = 0;
+        var right = numsCopy.Length - 1;
+        while(left < right) {
+            var ans = numsCopy[left] + numsCopy[right];
+            if(ans == target)
+                break;
+            else if(ans > target)
+                right--;
+            else
+                left++;
+        }
+        if(left == right)
+            return new int[0];
+        var lFlag = false;
+        var rFlag = false;
+        var ansLeft = 0;
+        var ansRight = 0;
+        for(int i = 0; i < nums.Length; i++) {
+            if(nums[i] == numsCopy[left] && !lFlag) {
+                ansLeft = i;
+                lFlag = true;
+            }
+            else if(nums[i] == numsCopy[right] && !rFlag) {
+                ansRight = i;
+                rFlag = true;
+            }
+        }
+        if(ansLeft > ansRight)
+            return new int[] { ansRight, ansLeft };
+        else
+            return new int[] { ansLeft, ansRight };
+    }
 }
 // @lc code=end
 
