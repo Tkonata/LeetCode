a1e917bc22ad87cb91c556167e220a32468329df
wakou <20671600+wakou@users.noreply.github.com>
2023-04-19 01:37:56 +0800

    答题文件存放在不同文件夹中

---
+++ csharp/medium/114.flatten-binary-tree-to-linked-list.cs
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=114 lang=csharp
+ *
+ * [114] Flatten Binary Tree to Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public void Flatten(TreeNode root) {
+        while(root != null) {
+            if(root.left == null)
+                root = root.right;
+            else {
+                TreeNode pre = root.left;
+                while(pre.right != null) {
+                    pre = pre.right;
+                }
+                pre.right = root.right;
+                root.right = root.left;
+                root.left = null;
+                root = root.right;
+            }
+        }
+    }
+}
+// @lc code=end
+
